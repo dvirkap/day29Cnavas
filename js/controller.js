2328661934ee497409899ad01a4f4c1e18c187b7
Sarel <Sarelk@gmail.com>
2019-02-13 14:49:53 +0200

    Onclick canvas

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,12 @@
 var gCanvas;
 var gCdraw;
+
 function init() {
     gCanvas = document.querySelector('#sarelDvir');
     gCdraw = gCanvas.getContext('2d');
 }
 
+function canvasClicked(ev) {
+    console.log("work");
+    console.log(ev);
+}
