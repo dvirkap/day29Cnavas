07853788c61c0cbe622f0958df4d05f12a4fd066
dvirkap <37699331+dvirkap@users.noreply.github.com>
2019-02-13 18:35:05 +0200

    triangle and draw line WORKING ;-)

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,6 @@
 'use strict'
+var x = 0;
+var y = 0;
 var isMouseDown = false;
 
 function init() {
@@ -7,8 +9,10 @@ function init() {
 }
 
 function canvasClicked(ev) {
-   if (gUserPrefs.isSaved === true) {
+    [x, y] = [ev.offsetX, ev.offsetY]
+    if (gUserPrefs.isSaved === true) {
        isMouseDown = true;
+       
    } else {
     gCdraw.font = '15px serif';
     gCdraw.fillText('You got to save your prefrences before you draw', 10, 50);
@@ -16,13 +20,23 @@ function canvasClicked(ev) {
     
 
 }
+function triangle() {
+    gCdraw.beginPath();
+    gCdraw.moveTo(75, 50);
+    gCdraw.lineTo(100, 75);
+    gCdraw.lineTo(100, 25);
+    // gCdraw.moveTo(75, 50);
+    // gCdraw.lineTo(100, 75);
+    // gCdraw.lineTo(100, 25);
+    gCdraw.fill();
+}
 
 function mouseMove(ev) {
     console.log(ev);
     // chooseFavColor(ev);
     if (isMouseDown && ev.buttons === 1) {
         gCdraw.fillStyle = gUserPrefs.strokeColor;
-
+        
         if (gUserPrefs.shape === "rect") {
             gCdraw.fillRect(ev.offsetX, ev.offsetY, 100, 100);
         }
@@ -32,14 +46,28 @@ function mouseMove(ev) {
             gCdraw.stroke();
         }
         if (gUserPrefs.shape === "triangle") {
+            var lineXPos = ev.offsetX;
+            var lineYPos = ev.offsetY;
             gCdraw.beginPath();
-            gCdraw.moveTo(75, 50);
-            gCdraw.lineTo(100, 75);
-            gCdraw.lineTo(100, 25);
-            // gCdraw.moveTo(75, 50);
-            // gCdraw.lineTo(100, 75);
-            // gCdraw.lineTo(100, 25);
+            gCdraw.moveTo(lineXPos, lineYPos);
+            gCdraw.lineTo(lineXPos + 100, lineYPos + 25);
+            gCdraw.lineTo(lineXPos + 25, lineYPos + 105);
             gCdraw.fill();
+            gCdraw.stroke();
+            // [x, y] = [lineXPos, lineYPos]
+            
+            }
+            
+            if (gUserPrefs.shape === "line") {
+            var lineXPos = ev.offsetX;
+            var lineYPos = ev.offsetY;
+            gCdraw.beginPath();
+            gCdraw.moveTo(x, y);
+            gCdraw.lineTo(lineXPos,lineYPos);
+            gCdraw.stroke();
+            [x, y] = [lineXPos, lineYPos];
+
+            
         }
 
     }
